5674f096f112a50af0ddd9915359dc502c926ee5
eoludotun <eoludotun@users.noreply.github.com>
2018-02-10 10:27:13 -0500

    Update subnetCalculator.ps1

--- subnetCalculator.ps1
+++ subnetCalculator.ps1
@@ -1,8 +1,8 @@
 <#
    .EXAMPLE 
-   subnetCalculator.ps1 -IPAddress 10.100.100.1 -NetMask 255.255.255.0 
-   subnetCalculator.ps1 -IPAddress 10.100.100.1 -NetMask 255.255.0.0 
-   subnetCalculator.ps1 -IPAddress 10.100.100.1 -NetMask 255.0.0.0
+    .\subnetCalculator.ps1 -IPAddress 10.100.100.1 -NetMask 255.255.255.0 
+    .\subnetCalculator.ps1 -IPAddress 10.100.100.1 -NetMask 255.255.0.0 
+    .\subnetCalculator.ps1 -IPAddress 10.100.100.1 -NetMask 255.0.0.0
 #>
 
 
